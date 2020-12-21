bba7fca814495bc68bb2e7f1a7d51fc7e1d69a5f
Arnaud Verstuyf <7952852+averstuyf@users.noreply.github.com>
2020-12-21 10:46:50 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,92 @@
+# Sync to Remote
+
+This shell script syncs local folders to a remote storage.
+
+## Requirements
+
+Tested on MacOS Big Sur. Might work on other releases.
+
+## Todo
+
+- [ ] Make script self-updating on run
+- [ ] Move todo to GitHub project boards
+
+## Installation
+
+Install the package manager [Homebrew](https://brew.sh/).
+
+### Install Rclone
+
+```sh
+brew install rclone
+```
+
+### Configure Rclone
+
+- rclone.org
+- rclone.org/drive/ - Google Drive
+
+```sh
+rclone config
+```
+
+Choose the following options:
+
+- menu option -> new remote
+- name -> gdrive
+- type -> drive (Google Drive)
+- client_id -> <Google Cloud Project - Google Drive API auth>
+- client_secret -> <Google Cloud Project - Google Drive API auth>
+- scope -> 1
+- root_folder_id -> <empty>
+- service_account_file -> <empty>
+- Edit advanced config -> no
+- Use auto config -> no
+- Configure this as a team drive -> no for 'my drive', yes for a shared drive
+
+## Usage
+
+### Run from the command line
+
+```sh
+./sync-to-cloud.sh <command> <flags>
+```
+
+Use the command `help` to see an overview of available commands and flags.
+
+### Schedule as a launchd agent
+
+Make the project files accessible to all users of a machine by placing it in `/Users/Shared`. For a single user; a more limited location such as ~/Applications can be used.
+
+By making a shallow clone of the git repository (using `--depth 1`), only the most recent changes are downloaded.
+
+```sh
+git clone --depth 1 https://github.com/averstuyf/av-tool-sync-to-remote.git /Users/Shared/av-tool-sync-to-remote/
+```
+
+Make a symlink to the [launchd agent config](https://manpagez.com/man/5/launchd.plist/) file in `/Library/LaunchAgents` for all users or `~/Library/LaunchAgents` for a specific user.
+
+```sh
+ln -s /Users/Shared/av-tool-sync-to-remote/com.av.sync-to-remote.plist /Library/LaunchAgents/com.av.sync-to-remote.plist
+```
+
+Have launchd load the agent.
+
+```sh
+launchctl load /Library/LaunchAgents/com.av.sync-to-remote.plist
+```
+
+See script output and errors.
+
+```sh
+cat /tmp/sync-to-remote.stdout
+cat /tmp/sync-to-remote.stderr
+```
+
+## License
+
+This repository is [licensed](LICENSE.md) under the permissive MIT License.
+
+---
+
+All rights reserved (c) 2020 [Arnaud Verstuyf](https://github.com/averstuyf).
---
+++ sync-to-remote.sh
@@ -0,0 +1,123 @@
+#!/bin/sh
+VERSION="4"
+
+BACKUPS=(
+    projects::$HOME/projects
+    desktop::$HOME/Desktop
+    documents::$HOME/Documents
+    applications::$HOME/Applications)
+
+REMOTE_NAME="gdrive-backup"
+SYNC_EXCEPTIONS_CUSTOM_FILENAME="sync-exceptions"
+
+DEVICE_NAME=$(scutil --get ComputerName)
+USER_NAME=$USER
+DATE=$(date +"%Y-%m-%d")
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Make capital letters lower-case
+# Replace multiple consecutive dashes with a single dash
+# Place a dash in front of the first capital letter of a group of capital letters
+# Replace any space with a dash
+simplify_text() {
+    echo $1 \
+    | tr ' ' '-' \
+    | sed 's/\(.\)\([A-Z]\)/\1-\2/g' \
+    | sed 's/-\{2,\}/-/g' \
+    | tr '[:upper:]' '[:lower:]'
+}
+
+DEVICE_NAME_SIMPLE=$(simplify_text $DEVICE_NAME)
+USER_NAME_SIMPLE=$USER_NAME
+
+showHelp()
+{
+    echo "Backup v$VERSION"
+    echo 
+    echo "Usage:"
+    echo "  $0 [command] [flags]"
+    echo
+    echo "Available Commands:"
+    echo "  backup\tPerform backup"
+    echo "  device\tShow device information"
+    echo "  version\tShow version"
+    echo "  help\tShow available commands and flags"
+    echo
+    echo "Available Flags:"
+    echo "  -v --verbose\tVerbose output including all processed files"
+    exit 1 # Exit script after printing help
+}
+
+perform_backup () {
+    # Make sure the custom exceptions file exists
+    touch $SYNC_EXCEPTIONS_CUSTOM_FILENAME
+
+    printf "Backup device - name: %s, user: %s, date: %s\n" "$DEVICE_NAME" "$USER" "$DATE"
+    for backup in "${BACKUPS[@]}" ;
+    do
+        BACKUP_NAME="${backup%%:*}"
+        LOCAL_PATH="${backup##*:}"
+        REMOTE_PATH="$USER_NAME_SIMPLE/$DEVICE_NAME_SIMPLE/$BACKUP_NAME"
+
+        printf "Backup folder - time: %s, name: %s, source: %s, target: %s\n" "$(date +%T)" "$BACKUP_NAME" "$LOCAL_PATH" "$REMOTE_PATH"
+
+        # Skip if source path does not exists
+        if [ ! -d "$LOCAL_PATH" ]; 
+        then
+            printf "Source not found - skip.\n" "$BACKUP_NAME" "$LOCAL_PATH"
+            continue
+        fi
+        
+        # Plist requires absolute path to app
+        # Dedupe first as 
+        # 1. Google Drive supports multiple files with the same name co-existing
+        # 2. Rclone sees duplicate files as errors during sync
+        /usr/local/bin/rclone dedupe $REMOTE_NAME:$REMOTE_PATH/ --fast-list --dedupe-mode oldest $*
+        /usr/local/bin/rclone sync "$LOCAL_PATH" $REMOTE_NAME:$REMOTE_PATH/ --backup-dir gdrive-backup:REMOTE_PATH-$DATE/ --create-empty-src-dirs --links --stats 10s --transfers 16 --drive-chunk-size 32M --checkers 32 --fast-list --exclude-from $SCRIPT_DIR/sync-exceptions-default --exclude-from $SCRIPT_DIR/$SYNC_EXCEPTIONS_CUSTOM_FILENAME $*
+
+        # Exit if rclone returned an error
+        #[ $? -ne 0 ] && exit $?
+
+        printf "\n"
+    done
+}
+
+#  The -t test option checks whether the stdin, [ -t 0 ],
+#+ or stdout, [ -t 1 ], in a given script is running in a terminal.
+if [ -t 0 ]
+then # Interactive
+    case $1 in
+        backup)
+            perform_backup --progress $2 $3 $4 $5 $6 $7 $8 $9
+            ;;
+        device)
+            printf "About local:\n"
+            printf "  Device name: %s\n" "$DEVICE_NAME"
+            printf "  Simple device name: %s\n" "$DEVICE_NAME_SIMPLE"
+            printf "  User name: %s\n" "$USER"
+            printf "  Simple user name: %s\n" "$USER_NAME_SIMPLE"
+            printf "  Date: %s\n" "$DATE"
+            printf "  Script directory: %s\n" "$SCRIPT_DIR"
+            echo
+            printf "About remote:\n"
+            printf "  Device name: %s\n" "$REMOTE_NAME"
+            rclone about $REMOTE_NAME:
+            ;;
+        version)
+            echo "Backup v$VERSION"
+            ;;
+        *)
+            showHelp
+    esac
+else # Non-interactive
+    echo "Switch to non-interactive mode"
+
+    # Guard against running on battery power
+    IS_AC_POWER = $(pmset -g ps | grep -c 'AC Power') # Power: 0 is battery; 1 is charger
+    if [ $IS_AC_POWER -eq 0 ] ; then
+        echo "Device on battery - exit.\n"
+        exit 0
+    fi
+    
+    perform_backup
+fi
